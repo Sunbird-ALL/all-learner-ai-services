82dc4ac322c3b140e7e4c95551796eee52fe3952
Sudeep Ratnaparkhe <sudeeppr1998@gmail.com>
2023-12-26 12:40:25 +0530

    increased incoming payload size limit

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,14 @@ async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule.forRoot(process.env.DATABASE),
     new FastifyAdapter({ logger: true }),
+    { rawBody: true }
   );
 
-  await app.register(fastifyMultipart, { attachFieldsToBody: 'keyValues' });
+  await app.register(fastifyMultipart, {
+    attachFieldsToBody: 'keyValues', limits: {
+      fileSize: 102400000,
+    },
+  });
 
   const config = new DocumentBuilder().setTitle('ALL Learner AI')
     .setDescription('ALL Learner AI API Spec Documentation \n\n The recommended the below flow to test:- \n 1. start using updateLearnerProfile API \n user_id & session_id are free text, that you can fill as you want and please refer them in the subsequent APIs where you use them to extract the LearnerProfile for that particular user id or session id levels. \n e.g., I have given few sample user ids and their respective session ids to play with. ideally session id needs to be unique for every time, but is not mandated now. given a user_id and session_id the profile vector will be unique. \n 2. For all Get APIs like -> GetTargets/Session/{sessionId}, GetTargets/User/{userId} use the sample session ids provided or you can use the ones that you used in updateLearnerProfile API call \n Provided below are some user Id and respective session Id to use in Get APIs or further update of learnerprofile')
@@ -30,6 +35,8 @@ async function bootstrap() {
     methods: ['GET', 'POST', 'HEAD', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: false,
   });
+
+  app.useBodyParser('application/json', { bodyLimit: 100 * 1000 * 1024 });
 
   await app.listen(process.env.PORT, '0.0.0.0');
 }
